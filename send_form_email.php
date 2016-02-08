225145de88608f5860f0de476d952aaa683c87fd
Godly T Alias <godlytalias@yahoo.co.in>
2016-02-09 00:47:50 +0530

    [Send Mail] Added database fetching
    Signed-off-by: Godly T Alias <godlytalias@yahoo.co.in>

--- send_form_email.php
+++ send_form_email.php
@@ -11,81 +11,100 @@ $mail->Host = 'smtp.mail.yahoo.com';
 $mail->Port = 587;
 $mail->SMTPSecure = 'tls';
 $mail->SMTPAuth = true;
-$mail->Username = "USERNAME";  //username
-$mail->Password = "PASSWORD";  //password
-  
- 
+$mail->Username = "username";  //username
+$mail->Password = "password";  //password
+
+$from_mail = "from_mail";
+$from_name = "from_name";
+$test_mail_id = "test_mail_id";
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "db_test";
+$tablename = "maildb";
      
  
-    function died($error) {
-        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-        echo "These errors appear below.<br /><br />"; 
-        echo $error."<br /><br />"; 
-        echo "Please go back and fix these errors.<br /><br />";
-        die();
-     }
+function died($error) {
+	echo $error."<br /><br />"; 
+	echo "Please go back and fix these errors.<br /><br />";
+	die();
+}
 
-    if(!isset($_POST['first_name']) ||
-       !isset($_POST['last_name']) ||
-       !isset($_POST['email']) ||
-       !isset($_POST['contents'])) {
-       died('We are sorry, but there appears to be a problem with the form you submitted.');       
-    }
- 
-    $first_name = $_POST['first_name']; // required
-    $last_name = $_POST['last_name']; // required
-    $email_from = $_POST['email']; // required
-    $contents = $_POST['contents']; // required
-     
- 
-    $error_message = "";
-    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-  if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
-  }
-    $string_exp = "/^[A-Za-z .'-]+$/";
-  if(!preg_match($string_exp,$first_name)) {
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
-  }
-  if(!preg_match($string_exp,$last_name)) {
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
-  }
-  if(strlen($contents) < 2) {
-    $error_message .= 'The contents you entered do not appear to be valid.<br />';
-  }
-  if(strlen($error_message) > 0) {
-    died($error_message);
-  }
- 
-    $email_message = "Form details below.\n\n";
- 
-     
- 
-    function clean_string($string) {
-       $bad = array("content-type","bcc:","to:","cc:","href");
-       return str_replace($bad,"",$string);
-     }
- 
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
-    $email_message .= "Email: ".clean_string($email_from)."\n";
-    $email_message .= "contents: ".clean_string($contents)."\n";
+if(!isset($_POST['content'])) {
+	died('Incomplete contents');       
+}
+if(!isset($_POST['subject'])) {
+	died('Subject field is empty.');       
+}
 
- 
-$mail->setFrom('godlytalias@yahoo.com', '');
-$mail->addAddress($email_from, $first_name);
-$mail->Subject = $contents;
-$mail->msgHTML(file_get_contents('emailform.html'), dirname(__FILE__));
-$mail->AltBody = 'This is a plain-text message body';
+$contents = $_POST['content']; // required
+$subject = $_POST['subject'];
 
-	for ($x = 0; $x < 10; $x++) {
-		$mail->addAddress($email_from, $first_name);
-		//send the message, check for errors
-		if (!$mail->send()) {
- 		   echo "Mailer Error: " . $mail->ErrorInfo;
-		} else {
-  		  echo "Message sent!";
-		}
+$error_message = "";
+
+if(strlen($contents) < 2) {
+	$error_message .= 'The contents you entered do not appear to be valid.<br />';
+}
+if(strlen($error_message) > 0) {
+	died($error_message);
+}
+ 
+$mail->setFrom($from_mail, $from_name);
+$mail->Subject = $subject;
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');
+$mail->AltBody = 'To view the message, please use an HTML compatible email viewer!';
+
+
+if ($_POST['testing'] == "testing")
+{
+	$name = "Tester";
+	$email = $test_mail_id;
+	$mailbody = "<p>Hi ". $name . ", </p>" . $contents;
+	$mail->msgHTML($mailbody);
+	$mail->addAddress($email, $name);
+	//send the message, check for errors
+	if (!$mail->send()) {
+		echo "Mailer Error: " . $mail->ErrorInfo;
+	} else {
+		echo "Message sent!";
 	}
- 
+	$mail->clearAddresses();
+}
+else
+{
+	// Create connection
+	$conn = new mysqli($servername, $username, $password);
+	mysqli_select_db($conn, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+		die("Database Connection failed: " . $conn->connect_error);
+	} 
+	echo "Database Connected successfully";
+
+	$sql = "SELECT firstname, email FROM " . $tablename;
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			$name = $row['firstname'];
+			$email = $row['email'];
+			if (strlen($name) < 1) $name = "Friend";
+			$mailbody = "<p>Hi ". $name . ", </p>" . $contents;
+			$mail->msgHTML($mailbody);
+			$mail->addAddress($email, $name);
+			//send the message, check for errors
+			if (!$mail->send()) {
+				echo "Mailer Error: " . $mail->ErrorInfo;
+			} else {
+				echo "Message sent!";
+			}
+			$mail->clearAddresses();
+		}
+	} else {
+		echo "NO MAILS FOUND";
+	}
+}
+$conn->close();
 ?>
